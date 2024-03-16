4ee2b3f7a6af1692c1ba750494bd1a10c188cbdb
Jan Kazubski <jkazubski@icloud.com>
2024-03-16 18:00:44 +0100

    Created project

---
+++ Pokedex/Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Jan Kazubski on 16/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
