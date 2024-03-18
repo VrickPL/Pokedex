VrickPL/Pokedex

commit 06d07d01042183f5984563cfdcd167c85fc0aba6

Improved methods responsible for getting pokemons from api

Pokedex/Pokedex/ViewModels/ChoosePokemonViewModel.swift
@@ -22,7 +22,7 @@ class ChoosePokemonViewModel: ObservableObject {
     private var offset: Int = 0
     private var limit: Int = 20
 
-    func updatePokemons() throws {
+    func updatePokemons() async throws {
         let endpoint =  if next.isEmpty {
             "https://pokeapi.co/api/v2/pokemon?offset=\(offset)&limit=\(limit)"
         } else {
@@ -33,73 +33,46 @@ class ChoosePokemonViewModel: ObservableObject {
             throw PokemonError.invalidUrl
         }
         
-        var errorToThrow: Error?
-
-        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, response, error in
-            if error == nil {
-                if data != nil {
-                    do {
-                        let decoder = JSONDecoder()
-                        decoder.keyDecodingStrategy = .convertFromSnakeCase
-                        let myServiceResponse = try decoder.decode(PokemonsData.self, from: data!)
-                        DispatchQueue.main.async {
-                            self.results.append(contentsOf: myServiceResponse.results)
-                            self.maxCount = myServiceResponse.count
-                            self.next = myServiceResponse.next
-                            self.myCount += myServiceResponse.results.count
-                        }
-                    } catch let exception {
-                        errorToThrow = exception
-                    }
-                } else {
-                    errorToThrow = PokemonError.invalidData
-                }
-            } else {
-                errorToThrow = PokemonError.invalidResponse
-            }
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+            throw PokemonError.invalidResponse
         }
+        
+        do {
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            let myServiceResponse = try decoder.decode(PokemonsData.self, from: data)
 
-        if let error = errorToThrow {
-            throw error
+            self.results.append(contentsOf: myServiceResponse.results)
+            self.maxCount = myServiceResponse.count
+            self.next = myServiceResponse.next
+            self.myCount += myServiceResponse.results.count
+        } catch {
+            throw PokemonError.invalidData
         }
-
-        task.resume()
     }
     
-    func findOnePokemon() throws {
+    func findOneNewPokemon() async throws {
+        self.pokemonFound = nil
         let endpoint = "https://pokeapi.co/api/v2/pokemon/\(searchTerm.lowercased())/"
 
         guard let url = URL(string: endpoint) else {
             throw PokemonError.invalidUrl
         }
         
-        var errorToThrow: Error?
-
-        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, response, error in
-            if error == nil {
-                if data != nil {
-                    do {
-                        let decoder = JSONDecoder()
-                        decoder.keyDecodingStrategy = .convertFromSnakeCase
-                        let myServiceResponse = try decoder.decode(Pokemon.self, from: data!)
-                        DispatchQueue.main.async {
-                            self.pokemonFound = myServiceResponse
-                        }
-                    } catch let exception {
-                        errorToThrow = exception
-                    }
-                } else {
-                    errorToThrow = PokemonError.invalidData
-                }
-            } else {
-                errorToThrow = PokemonError.invalidResponse
-            }
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+            throw PokemonError.invalidResponse
         }
-
-        if let error = errorToThrow {
-            throw error
+        
+        do {
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            self.pokemonFound = try decoder.decode(Pokemon.self, from: data)
+        } catch {
+            throw PokemonError.invalidData
         }
-
-        task.resume()
     }
 }

Pokedex/Pokedex/Views/ChoosePokemonView.swift
@@ -20,16 +20,7 @@ struct ChoosePokemonView: View {
             ScrollView {
                 VStack {
                     if viewModel.results.isEmpty {
-                        LoadingView()
-                            .onAppear {
-                                do {
-                                    try viewModel.updatePokemons()
-                                } catch is PokemonError {
-                                    // TODO: implement toast
-                                } catch {
-                                    // unexpected
-                                }
-                            }
+                        LoadingPokemonsView(viewModel: viewModel)
                     } else {
                         if viewModel.searchTerm.isEmpty {
                             let pokemonWidth = UIScreen.main.bounds.width * 2 / 5
@@ -46,16 +37,7 @@ struct ChoosePokemonView: View {
                                 }
                                 
                                 if !viewModel.next.isEmpty {
-                                    LoadingView()
-                                        .onAppear {
-                                            do {
-                                                try viewModel.updatePokemons()
-                                            } catch is PokemonError {
-                                                // TODO: implement toast
-                                            } catch {
-                                                // unexpected
-                                            }
-                                        }
+                                    LoadingPokemonsView(viewModel: viewModel)
                                     LoadingView()
                                 }
                             }
@@ -100,20 +82,39 @@ struct ChoosePokemonView: View {
             )
             .onChange(of: viewModel.searchTerm) {
                 if search {
-                    do {
-                        try viewModel.findOnePokemon()
-                    } catch is PokemonError {
-                        // TODO: implement toast
-                    } catch {
-                        // unexpected
+                    Task {
+                        do {
+                            try await viewModel.findOneNewPokemon()
+                        } catch is PokemonError {
+                            // TODO: implement toast
+                        } catch {
+                            // unexpected
+                        }
                     }
                 }
             }
         }
     }
-}
     
-
+    private struct LoadingPokemonsView: View {
+        @ObservedObject var viewModel: ChoosePokemonViewModel
+        
+        var body: some View {
+            LoadingView()
+                .onAppear {
+                    Task {
+                        do {
+                            try await viewModel.updatePokemons()
+                        } catch is PokemonError {
+                            // TODO: implement toast
+                        } catch {
+                            // unexpected
+                        }
+                    }
+                }
+        }
+    }
+}
 
 #Preview {
     ChoosePokemonView()

commit a568b68d4a4bacd4c8db245a22e60cb287ff3293

Fixed not loading data, caused by commit: 06d07d0

Pokedex/Pokedex/ViewModels/ChoosePokemonViewModel.swift
@@ -18,61 +18,85 @@ class ChoosePokemonViewModel: ObservableObject {
         return results.filter{ $0.name.localizedCaseInsensitiveContains(searchTerm) }
     }
     @Published var pokemonFound: Pokemon?
-
+    
     private var offset: Int = 0
     private var limit: Int = 20
-
-    func updatePokemons() async throws {
-        let endpoint =  if next.isEmpty {
-            "https://pokeapi.co/api/v2/pokemon?offset=\(offset)&limit=\(limit)"
-        } else {
-            next
-        }
-
+    
+    func updatePokemons() throws {
+        let endpoint = next.isEmpty ? "https://pokeapi.co/api/v2/pokemon?offset=\(offset)&limit=\(limit)" : next
+        
         guard let url = URL(string: endpoint) else {
             throw PokemonError.invalidUrl
         }
         
-        let (data, response) = try await URLSession.shared.data(from: url)
+        var errorToThrow: Error?
         
-        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-            throw PokemonError.invalidResponse
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, response, error in
+            if error == nil {
+                if data != nil {
+                    do {
+                        let decoder = JSONDecoder()
+                        decoder.keyDecodingStrategy = .convertFromSnakeCase
+                        let myServiceResponse = try decoder.decode(PokemonsData.self, from: data!)
+                        DispatchQueue.main.async {
+                            self.results.append(contentsOf: myServiceResponse.results)
+                            self.maxCount = myServiceResponse.count
+                            self.next = myServiceResponse.next
+                            self.myCount += myServiceResponse.results.count
+                        }
+                    } catch let exception {
+                        errorToThrow = exception
+                    }
+                } else {
+                    errorToThrow = PokemonError.invalidData
+                }
+            } else {
+                errorToThrow = PokemonError.invalidResponse
+            }
         }
         
-        do {
-            let decoder = JSONDecoder()
-            decoder.keyDecodingStrategy = .convertFromSnakeCase
-            let myServiceResponse = try decoder.decode(PokemonsData.self, from: data)
-
-            self.results.append(contentsOf: myServiceResponse.results)
-            self.maxCount = myServiceResponse.count
-            self.next = myServiceResponse.next
-            self.myCount += myServiceResponse.results.count
-        } catch {
-            throw PokemonError.invalidData
+        if let error = errorToThrow {
+            throw error
         }
+        
+        task.resume()
     }
     
-    func findOneNewPokemon() async throws {
-        self.pokemonFound = nil
+    func findOnePokemon() throws {
         let endpoint = "https://pokeapi.co/api/v2/pokemon/\(searchTerm.lowercased())/"
-
+        
         guard let url = URL(string: endpoint) else {
             throw PokemonError.invalidUrl
         }
         
-        let (data, response) = try await URLSession.shared.data(from: url)
+        var errorToThrow: Error?
         
-        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-            throw PokemonError.invalidResponse
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, response, error in
+            if error == nil {
+                if data != nil {
+                    do {
+                        let decoder = JSONDecoder()
+                        decoder.keyDecodingStrategy = .convertFromSnakeCase
+                        let myServiceResponse = try decoder.decode(Pokemon.self, from: data!)
+                        DispatchQueue.main.async {
+                            self.pokemonFound = myServiceResponse
+                        }
+                    } catch let exception {
+                        errorToThrow = exception
+                    }
+                } else {
+                    errorToThrow = PokemonError.invalidData
+                }
+            } else {
+                errorToThrow = PokemonError.invalidResponse
+            }
         }
         
-        do {
-            let decoder = JSONDecoder()
-            decoder.keyDecodingStrategy = .convertFromSnakeCase
-            self.pokemonFound = try decoder.decode(Pokemon.self, from: data)
-        } catch {
-            throw PokemonError.invalidData
+        if let error = errorToThrow {
+            throw error
         }
+        
+        task.resume()
     }
 }
+

Pokedex/Pokedex/Views/ChoosePokemonView.swift
@@ -84,7 +84,7 @@ struct ChoosePokemonView: View {
                 if search {
                     Task {
                         do {
-                            try await viewModel.findOneNewPokemon()
+                            try viewModel.findOnePokemon()
                         } catch is PokemonError {
                             // TODO: implement toast
                         } catch {
@@ -102,14 +102,12 @@ struct ChoosePokemonView: View {
         var body: some View {
             LoadingView()
                 .onAppear {
-                    Task {
-                        do {
-                            try await viewModel.updatePokemons()
-                        } catch is PokemonError {
-                            // TODO: implement toast
-                        } catch {
-                            // unexpected
-                        }
+                    do {
+                        try viewModel.updatePokemons()
+                    } catch is PokemonError {
+                        // TODO: implement toast
+                    } catch {
+                        // unexpected
                     }
                 }
         }