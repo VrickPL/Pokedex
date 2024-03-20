a99f6557687f0b76cdb6deac63f4be9ebd70ee20
Jan Kazubski <jkazubski@icloud.com>
2024-03-21 00:01:59 +0100

    Added dependency of SimpleToast and added translations

---
+++ Pokedex/Pokedex/Views/ToastPopUpView.swift
@@ -0,0 +1,30 @@
+//
+//  ToastPopUpView.swift
+//  Pokedex
+//
+//  Created by Jan Kazubski on 20/03/2024.
+//
+
+import Foundation
+import SwiftUI
+import SimpleToast
+
+struct ToastPopUpView: View {
+    @State var text: String
+    @State var color: Color?
+    
+    var body: some View {
+        VStack {
+            Text(LocalizedStringKey(text))
+        }
+        .padding(20)
+        .background(color)
+        .opacity(0.8)
+        .foregroundColor(.white)
+        .cornerRadius(14)
+    }
+}
+
+#Preview {
+    ToastPopUpView(text: "success_add_pokemon", color: .green)
+}
