1a0b9ef04e65072d1b1e77e5b780bd6338c90f5d
Jan Kazubski <jkazubski@icloud.com>
2024-03-22 21:05:55 +0100

    Added background based on pokemon type

---
+++ Pokedex/Pokedex/Utils/Utils.swift
@@ -0,0 +1,26 @@
+//
+//  Utils.swift
+//  Pokedex
+//
+//  Created by Jan Kazubski on 22/03/2024.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    init(hex: String) {
+        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
+
+        var rgb: UInt64 = 0
+
+        Scanner(string: hexSanitized).scanHexInt64(&rgb)
+
+        let red = Double((rgb & 0xFF0000) >> 16) / 255.0
+        let green = Double((rgb & 0x00FF00) >> 8) / 255.0
+        let blue = Double(rgb & 0x0000FF) / 255.0
+
+        self.init(red: red, green: green, blue: blue)
+    }
+}
--- Pokedex/Pokedex/Views/DetailedPokemonView.swift
+++ Pokedex/Pokedex/Views/DetailedPokemonView.swift
@@ -34,118 +34,128 @@ struct DetailedPokemonView: View {
     
     
     var body: some View {
-        ZStack {
-            Color("BackgroundColor")
-                .ignoresSafeArea()
-            
-            VStack {
-                if couldntGetPokemon {
-                    Text("couldntGetPokemon")
-                        .foregroundStyle(Color.red)
-                } else if pokemon == nil {
-                    LoadingView()
-                } else {
-                    if let image = image {
-                        image
-                            .resizable()
-                            .scaledToFit()
-                            .padding()
-                    } else if couldntGetPokemonImage {
-                        PokemonImage(image: nil, width: pokemonWidth, isInMyPokedex: isInMyPokedex)
-                    } else {
+        NavigationView {
+            ScrollView {
+                VStack {
+                    if couldntGetPokemon {
+                        Text("couldntGetPokemon")
+                            .foregroundStyle(Color.red)
+                    } else if pokemon == nil {
                         LoadingView()
-                    }
-                    
-                    Text(pokemon!.name.capitalized)
-                        .font(.custom("PressStart2P-Regular", size: 18))
-                        .padding(.bottom)
-                    
-                    HStack {
-                        Text(LocalizedStringKey("weight")).bold()
-                        Text("\(pokemon!.weight / 10)kg")
-                        Spacer()
-                    }
-                    HStack {
-                        Text(LocalizedStringKey("height")).bold()
-                        Text("\(pokemon!.height * 10)cm")
-                        Spacer()
-                    }
-                    .padding(.top)
-                    .padding(.bottom)
-                    
-                    if isInMyPokedex {
-                        Button {
-                            self.toastOptions = .successDeletePokemon
-                            self.showToastDelete = true
-                        } label: {
-                            Text("remove_from_pokeball")
-                                .padding()
-                                .background(.white)
-                                .cornerRadius(10)
-                        }
                     } else {
-                        Button {
-                            self.toastOptions = .successAddPokemon
-                            self.showToastAdd = true
-                        } label: {
-                            Text("catch_in_pokeball")
+                        if let image = image {
+                            image
+                                .resizable()
+                                .scaledToFit()
                                 .padding()
-                                .background(.white)
-                                .cornerRadius(10)
+                        } else if couldntGetPokemonImage {
+                            PokemonImage(image: nil, width: pokemonWidth, isInMyPokedex: isInMyPokedex)
+                        } else {
+                            LoadingView()
+                        }
+                        
+                        Text(pokemon!.name.capitalized)
+                            .font(.custom("PressStart2P-Regular", size: 18))
+                            .padding(.bottom)
+                        
+                        HStack {
+                            Text(LocalizedStringKey("weight")).bold()
+                            Text("\(pokemon!.weight / 10)kg")
+                            Spacer()
+                        }
+                        HStack {
+                            Text(LocalizedStringKey("height")).bold()
+                            Text("\(pokemon!.height * 10)cm")
+                            Spacer()
+                        }
+                        .padding(.top)
+                        .padding(.bottom)
+                        
+                        if isInMyPokedex {
+                            Button {
+                                self.toastOptions = .successDeletePokemon
+                                self.showToastDelete = true
+                            } label: {
+                                Text("remove_from_pokeball")
+                                    .padding()
+                                    .background(.white)
+                                    .cornerRadius(10)
+                            }
+                        } else {
+                            Button {
+                                self.toastOptions = .successAddPokemon
+                                self.showToastAdd = true
+                            } label: {
+                                Text("catch_in_pokeball")
+                                    .padding()
+                                    .background(.white)
+                                    .cornerRadius(10)
+                            }
                         }
                     }
                 }
-            }
-            .padding()
-            .task {
-                do {
-                    if pokemon == nil {
-                        pokemon = try await DetailedPokemonViewModel(id: id).getPokemon()
-                    }
-                    self.couldntGetPokemon = false
-                    self.isInMyPokedex = FavouritePokemonsManager.shared.checkIfIsSaved(id)
-                } catch is PokemonError {
-                    self.couldntGetPokemon = true
-                } catch {
-                    self.toastOptions = .unexpectedError
-                    self.showToast = true
-                    self.couldntGetPokemon = true
-                }
-            }
-            .task {
-                if image == nil {
+                .padding()
+                .task {
                     do {
-                        self.image = try await PokemonImageViewModel(id: id).getBigPokemonImage()
-                        self.couldntGetPokemonImage = false
+                        if pokemon == nil {
+                            pokemon = try await DetailedPokemonViewModel(id: id).getPokemon()
+                        }
+                        self.couldntGetPokemon = false
+                        self.isInMyPokedex = FavouritePokemonsManager.shared.checkIfIsSaved(id)
                     } catch is PokemonError {
-                        self.couldntGetPokemonImage = true
+                        self.couldntGetPokemon = true
                     } catch {
                         self.toastOptions = .unexpectedError
                         self.showToast = true
-                        self.couldntGetPokemonImage = true
+                        self.couldntGetPokemon = true
                     }
                 }
+                .task {
+                    if image == nil {
+                        do {
+                            self.image = try await PokemonImageViewModel(id: id).getBigPokemonImage()
+                            self.couldntGetPokemonImage = false
+                        } catch is PokemonError {
+                            self.couldntGetPokemonImage = true
+                        } catch {
+                            self.toastOptions = .unexpectedError
+                            self.showToast = true
+                            self.couldntGetPokemonImage = true
+                        }
+                    }
+                }
+                .onChange(of: favouritePokemons) {
+                    self.isInMyPokedex = FavouritePokemonsManager.shared.checkIfIsSaved(id)
+                }
+                .simpleToast(isPresented: $showToast, options: getToastConfig(), onDismiss: {}) {
+                    ToastPopUpView(text: toastOptions.rawValue, color: toastOptions.getColor())
+                }
+                .simpleToast(isPresented: $showToastAdd, options: getToastConfig(), onDismiss: {
+                    FavouritePokemonsManager.shared.addPokemonId(id)
+                }) {
+                    ToastPopUpView(text: toastOptions.rawValue, color: toastOptions.getColor())
+                }
+                .simpleToast(isPresented: $showToastDelete, options: getToastConfig(), onDismiss: {
+                    FavouritePokemonsManager.shared.removePokemonId(id)
+                }) {
+                    ToastPopUpView(text: toastOptions.rawValue, color: toastOptions.getColor())
+                }
             }
-            .onChange(of: favouritePokemons) {
-                self.isInMyPokedex = FavouritePokemonsManager.shared.checkIfIsSaved(id)
-            }
-            .simpleToast(isPresented: $showToast, options: getToastConfig(), onDismiss: {}) {
-                ToastPopUpView(text: toastOptions.rawValue, color: toastOptions.getColor())
-            }
-            .simpleToast(isPresented: $showToastAdd, options: getToastConfig(), onDismiss: {
-                FavouritePokemonsManager.shared.addPokemonId(id)
-            }) {
-                ToastPopUpView(text: toastOptions.rawValue, color: toastOptions.getColor())
-            }
-            .simpleToast(isPresented: $showToastDelete, options: getToastConfig(), onDismiss: {
-                FavouritePokemonsManager.shared.removePokemonId(id)
-            }) {
-                ToastPopUpView(text: toastOptions.rawValue, color: toastOptions.getColor())
-            }
+            .background(getBackgroundColor())
+        }
+    }
+    
+    private func getBackgroundColor() -> Color {
+        if couldntGetPokemon || pokemon == nil {
+            return Color("BackgroundColor")
+        } else if let color = pokemon!.getColorForType().map({ Color(hex: $0) }) {
+            return color.opacity(0.6)
+        } else {
+            return Color("BackgroundColor")
         }
     }
 }
 
 #Preview {
-    DetailedPokemonView(id: 91)
+    DetailedPokemonView(id: 1)
 }
